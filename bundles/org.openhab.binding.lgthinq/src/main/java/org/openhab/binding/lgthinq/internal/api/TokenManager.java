7c78dd345d70f49eedf7c5fd9b5f23ee91c30ad0
Nemer Daud <nemer.daud@gmail.com>
2022-01-28 19:34:59 -0300

    [lgthinq] Initial Contribution. Binding to integrate OpenHab to LG Thinq API to control Thinq compatible devices through OpenHab. Signed-off-by: Nemer Daud <nemer.daud@gmail.com>
    Signed-off-by: nemerdaud <nemer.daud@gmail.com>

---
+++ bundles/org.openhab.binding.lgthinq/src/main/java/org/openhab/binding/lgthinq/internal/api/TokenManager.java
@@ -0,0 +1,158 @@
+/**
+ * Copyright (c) 2010-2022 Contributors to the openHAB project
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ */
+package org.openhab.binding.lgthinq.internal.api;
+
+import static org.openhab.binding.lgthinq.internal.LGThinqBindingConstants.GATEWAY_URL;
+import static org.openhab.binding.lgthinq.internal.LGThinqBindingConstants.THINQ_CONNECTION_DATA_FILE;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Objects;
+
+import org.eclipse.jdt.annotation.NonNull;
+import org.eclipse.jdt.annotation.NonNullByDefault;
+import org.eclipse.jdt.annotation.Nullable;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.openhab.binding.lgthinq.internal.errors.*;
+
+/**
+ * The {@link TokenManager} Principal facade to manage all token handles
+ *
+ * @author Nemer Daud - Initial contribution
+ */
+@NonNullByDefault
+public class TokenManager {
+    private static final int EXPIRICY_TOLERANCE_SEC = 60;
+    private final OauthLgEmpAuthenticator oAuthAuthenticator;
+    private final ObjectMapper objectMapper = new ObjectMapper();
+    @Nullable
+    private TokenResult tokenCached;
+    private static final TokenManager instance;
+    static {
+        instance = new TokenManager();
+    }
+
+    private TokenManager() {
+        oAuthAuthenticator = OauthLgEmpAuthenticator.getInstance();
+    }
+
+    public static TokenManager getInstance() {
+        return instance;
+    }
+
+    public boolean isTokenExpired(TokenResult token) {
+        Calendar c = Calendar.getInstance();
+        c.setTime(token.getGeneratedTime());
+        c.add(Calendar.SECOND, token.getExpiresIn() - EXPIRICY_TOLERANCE_SEC);
+        Date expiricyDate = c.getTime();
+        return expiricyDate.before(new Date());
+    }
+
+    @NonNull
+    public TokenResult refreshToken(String bridgeName, TokenResult currentToken) throws RefreshTokenException {
+        try {
+            TokenResult token = oAuthAuthenticator.doRefreshToken(currentToken);
+            objectMapper.writeValue(new File(getConfigDataFileName(bridgeName)), token);
+            return token;
+        } catch (IOException e) {
+            throw new RefreshTokenException("Error refreshing LGThinq token", e);
+        }
+    }
+
+    private String getConfigDataFileName(String bridgeName) {
+        return String.format(THINQ_CONNECTION_DATA_FILE, bridgeName);
+    }
+
+    public boolean isOauthTokenRegistered(String bridgeName) {
+        File tokenFile = new File(getConfigDataFileName(bridgeName));
+        // TODO - check if the file content is valid.
+        return tokenFile.isFile();
+    }
+
+    public void oauthFirstRegistration(String bridgeName, String language, String country, String username,
+            String password)
+            throws LGGatewayException, PreLoginException, AccountLoginException, TokenException, IOException {
+        Gateway gw;
+        OauthLgEmpAuthenticator.PreLoginResult preLogin;
+        OauthLgEmpAuthenticator.LoginAccountResult accountLogin;
+        TokenResult token;
+        UserInfo userInfo;
+        try {
+            gw = oAuthAuthenticator.discoverGatewayConfiguration(GATEWAY_URL, language, country);
+        } catch (Exception ex) {
+            throw new LGGatewayException("Error trying to discovery the LG Gateway Setting for the region informed",
+                    ex);
+        }
+        try {
+            preLogin = oAuthAuthenticator.preLoginUser(gw, username, password);
+        } catch (Exception ex) {
+            throw new PreLoginException("Error doing pre-login of the user in the Emp LG Server", ex);
+        }
+        try {
+            accountLogin = oAuthAuthenticator.loginUser(gw, preLogin);
+        } catch (Exception ex) {
+            throw new AccountLoginException("Error doing user's account login on the Emp LG Server", ex);
+        }
+        try {
+            token = oAuthAuthenticator.getToken(gw, accountLogin);
+        } catch (Exception ex) {
+            throw new TokenException("Error getting Token", ex);
+        }
+        try {
+            userInfo = oAuthAuthenticator.getUserInfo(token);
+            token.setUserInfo(userInfo);
+            token.setGatewayInfo(gw);
+        } catch (Exception ex) {
+            throw new TokenException("Error getting UserInfo from Token", ex);
+        }
+
+        // persist the token information generated in file
+        objectMapper.writeValue(new File(getConfigDataFileName(bridgeName)), token);
+    }
+
+    public TokenResult getValidRegisteredToken(String bridgeName) throws IOException, RefreshTokenException {
+        @NonNull
+        TokenResult validToken;
+        if (tokenCached == null) {
+            tokenCached = objectMapper.readValue(new File(getConfigDataFileName(bridgeName)), TokenResult.class);
+        }
+
+        if (!isValidToken(tokenCached)) {
+            throw new RefreshTokenException(
+                    "Token is not valid. Try to delete token file and disable/enable bridge to restart authentication process");
+        }
+        validToken = Objects.requireNonNull(tokenCached, "Unexpected. Never null here");
+        if (isTokenExpired(validToken)) {
+            validToken = refreshToken(bridgeName, validToken);
+        }
+        return validToken;
+    }
+
+    private boolean isValidToken(@Nullable TokenResult token) {
+        return token != null && !token.getAccessToken().isBlank() && token.getExpiresIn() != 0
+                && !token.getOauthBackendUrl().isBlank() && !token.getRefreshToken().isBlank();
+    }
+
+    /**
+     * Remove the toke file registered for the bridge. Must be called only if the bridge is removed
+     */
+    public void cleanupTokenRegistry(String bridgeName) {
+        File f = new File(getConfigDataFileName(bridgeName));
+        if (f.isFile()) {
+            f.delete();
+        }
+    }
+}
---
+++ bundles/org.openhab.binding.lgthinq/src/main/java/org/openhab/binding/lgthinq/internal/discovery/LGThinqDiscoveryService.java
@@ -0,0 +1,156 @@
+/**
+ * Copyright (c) 2010-2022 Contributors to the openHAB project
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ */
+package org.openhab.binding.lgthinq.internal.discovery;
+
+import static org.openhab.binding.lgthinq.internal.LGAirConditionerHandler.THING_TYPE_AIR_CONDITIONER;
+import static org.openhab.binding.lgthinq.internal.LGThinqBindingConstants.*;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+
+import org.eclipse.jdt.annotation.NonNullByDefault;
+import org.eclipse.jdt.annotation.Nullable;
+import org.openhab.binding.lgthinq.internal.LGAirConditionerHandler;
+import org.openhab.binding.lgthinq.internal.errors.LGThinqException;
+import org.openhab.binding.lgthinq.internal.handler.LGBridgeHandler;
+import org.openhab.binding.lgthinq.lgapi.LGApiClientService;
+import org.openhab.binding.lgthinq.lgapi.LGApiV1ClientServiceImpl;
+import org.openhab.binding.lgthinq.lgapi.LGApiV2ClientServiceImpl;
+import org.openhab.binding.lgthinq.lgapi.model.LGDevice;
+import org.openhab.core.config.discovery.AbstractDiscoveryService;
+import org.openhab.core.config.discovery.DiscoveryResult;
+import org.openhab.core.config.discovery.DiscoveryResultBuilder;
+import org.openhab.core.config.discovery.DiscoveryService;
+import org.openhab.core.thing.Thing;
+import org.openhab.core.thing.ThingTypeUID;
+import org.openhab.core.thing.ThingUID;
+import org.openhab.core.thing.binding.ThingHandler;
+import org.openhab.core.thing.binding.ThingHandlerService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * The {@link LGThinqDiscoveryService}
+ *
+ * @author Nemer Daud - Initial contribution
+ */
+@NonNullByDefault
+public class LGThinqDiscoveryService extends AbstractDiscoveryService implements DiscoveryService, ThingHandlerService {
+
+    private final Logger logger = LoggerFactory.getLogger(LGThinqDiscoveryService.class);
+    private @Nullable LGBridgeHandler bridgeHandler;
+    private @Nullable ThingUID bridgeHandlerUID;
+    private final LGApiClientService lgApiV1ClientService, lgApiV2ClientService;
+
+    public LGThinqDiscoveryService() {
+        super(LGAirConditionerHandler.SUPPORTED_THING_TYPES, SEARCH_TIME);
+        lgApiV1ClientService = LGApiV1ClientServiceImpl.getInstance();
+        lgApiV2ClientService = LGApiV2ClientServiceImpl.getInstance();
+    }
+
+    @Override
+    protected void startScan() {
+    }
+
+    @Override
+    public void setThingHandler(@Nullable ThingHandler handler) {
+        if (handler instanceof LGBridgeHandler) {
+            bridgeHandler = (LGBridgeHandler) handler;
+            bridgeHandlerUID = handler.getThing().getUID();
+        }
+    }
+
+    @Override
+    public @Nullable ThingHandler getThingHandler() {
+        return bridgeHandler;
+    }
+
+    @Override
+    public void activate() {
+        if (bridgeHandler != null) {
+            bridgeHandler.registerDiscoveryListener(this);
+        }
+    }
+
+    @Override
+    public void deactivate() {
+        ThingHandlerService.super.deactivate();
+    }
+
+    public void removeLgDeviceDiscovery(LGDevice device) {
+        try {
+            ThingUID thingUID = getThingUID(device);
+            thingRemoved(thingUID);
+        } catch (LGThinqException e) {
+            logger.error("Error getting Thing UID");
+        }
+    }
+
+    public void addLgDeviceDiscovery(String bridgeName, LGDevice device) {
+        String modelId = device.getModelName();
+        ThingUID thingUID;
+        ThingTypeUID thingTypeUID;
+        try {
+            thingUID = getThingUID(device);
+            thingTypeUID = getThingTypeUID(device);
+        } catch (LGThinqException e) {
+            logger.debug("Discovered unsupported LG device of type '{}' and model '{}' with id {}",
+                    device.getDeviceType(), modelId, device.getDeviceId());
+            return;
+        }
+
+        Map<String, Object> properties = new HashMap<>();
+        properties.put(DEVICE_ID, device.getDeviceId());
+        properties.put(DEVICE_ALIAS, device.getAlias());
+        properties.put(MODEL_URL_INFO, device.getModelJsonUri());
+        properties.put(PLATFORM_TYPE, device.getPlatformType());
+        try {
+            // registry the capabilities of the thing
+            if (PLATFORM_TYPE_V1.equals(device.getPlatformType())) {
+                lgApiV1ClientService.getDeviceCapability(bridgeName, device.getModelJsonUri(), true);
+            } else {
+                lgApiV2ClientService.getDeviceCapability(bridgeName, device.getModelJsonUri(), true);
+            }
+
+        } catch (Exception ex) {
+            logger.error(
+                    "Error trying to get device capabilities in discovery service. Fallback to the defaults values",
+                    ex);
+        }
+        properties.put(Thing.PROPERTY_MODEL_ID, modelId);
+
+        DiscoveryResult discoveryResult = DiscoveryResultBuilder.create(thingUID).withThingType(thingTypeUID)
+                .withProperties(properties).withBridge(bridgeHandlerUID).withRepresentationProperty(DEVICE_ID)
+                .withLabel(device.getAlias()).build();
+
+        thingDiscovered(discoveryResult);
+    }
+
+    private ThingUID getThingUID(LGDevice device) throws LGThinqException {
+        ThingTypeUID thingTypeUID = getThingTypeUID(device);
+        return new ThingUID(thingTypeUID,
+                Objects.requireNonNull(bridgeHandlerUID, "bridgeHandleUid should never be null here"),
+                device.getDeviceId());
+    }
+
+    private ThingTypeUID getThingTypeUID(LGDevice device) throws LGThinqException {
+        // Short switch, but is here because it is going to be increase after new LG Devices were added
+        switch (device.getDeviceType()) {
+            case AIR_CONDITIONER:
+                return THING_TYPE_AIR_CONDITIONER;
+            default:
+                throw new LGThinqException(String.format("device type [%s] not supported", device.getDeviceType()));
+        }
+    }
+}
